f69a98bb9642d4dddd733139819b4ece01f8d64e
Syauqi Ikmal Annabil <112393784+SyauqiIkmal@users.noreply.github.com>
2026-07-24 19:35:05 +0700

    chore: add tailwind css v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
+  css: ['@/assets/css/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
 })
